8a2e6f5dd19637604b6e3f412add0d82ca70889d
Donovan Mikrot <donovan@donovan.mn>
2015-04-06 00:00:37 -0500

    Add shield chest with switch and 20 rupee chest

--- data/maps/training_cave/cave_b1.lua
+++ data/maps/training_cave/cave_b1.lua
@@ -1,5 +1,22 @@
 local map = ...
 
-function door1_open_switch:on_activated()
+function map:on_started()
+  if shield_chest:is_open() then
+    shield_chest_switch:set_activated(true)
+  else
+    shield_chest:set_enabled(false)
+  end
+
+  door1_switch:set_activated(door1_left:is_open())
+  
+end
+
+function shield_chest_switch:on_activated()
+  sol.audio.play_sound("chest_appears")
+  shield_chest:set_enabled(true)
+end
+
+function door1_switch:on_activated()
   map:open_doors("door1_")
 end
+
